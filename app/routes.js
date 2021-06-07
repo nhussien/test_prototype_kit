9f6f6c6c9ac0371c97eee926a6d7ff04fb8eb183
Nadine Hussien <52565316+nhussien@users.noreply.github.com>
2021-06-07 16:34:51 +0100

    change routes file

--- app/routes.js
+++ app/routes.js
@@ -6,10 +6,10 @@ const router = express.Router()
 router.post('/juggling-balls-answer', function (req, res) {
 
   // Make a variable and give it the value from 'juggling-balls'
-  var jugglingBalls = req.session.data['juggling-balls']
+  var jugglingBalls = req.session.data['how-many-balls']
 
   // Check whether the variable matches a condition
-  if (jugglingBalls = "3 or more"){
+  if (jugglingBalls == "3 or more"){
     // Send user to next page
     res.redirect('/juggling-trick')
   } else {
